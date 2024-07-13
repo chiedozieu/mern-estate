89efe597ace1867ead3bfe2d55f378d85c81ebb4
chiedozieu <chiedozieugwor@gmail.com>
2024-07-13 19:30:30 +0100

    Pages and routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Logout from './pages/Logout'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div className='text-red-400'>
-     App 
-    </div>
+    <BrowserRouter>
+     <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/logout' element={<Logout />}/>
+        <Route path='/profile' element={<Profile />}/>
+         
+     </Routes>
+    </BrowserRouter>
   )
 }
 
