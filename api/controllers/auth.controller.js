2bba5fb45b36d2bc16a31723c91683504b9c6052
chiedozieu <chiedozieugwor@gmail.com>
2024-07-15 12:27:38 +0100

    complete Login page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import UserModel from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/errorHandler.js";
+import jwt from 'jsonwebtoken'
 
 export const SignUp = async (req, res, next) =>{
 try {
@@ -10,7 +12,7 @@ try {
     }
     const userExist = await UserModel.findOne({email})
     if(userExist){
-        throw new Error('User already exists')
+        throw new Error('User already exists')  // We can use the error handler we created here instead
     }
     const hashedPassword = bcryptjs.hashSync(password, 10)
 
@@ -28,4 +30,32 @@ try {
       next(error)
     }
     
+}
+
+
+
+export const SignIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body  
+        const validUser = await UserModel.findOne({email})
+        if(!validUser) return next(errorHandler(404, 'User not found'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials'));
+
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET_KEY);
+
+//Important to remove the password field before sending the json
+const { password: passW, ...rest } = validUser._doc
+        res.cookie('access_token', token, {httpOnly: true, secure: true}).json({
+            rest,
+            message: 'Login successful',
+            success: true,
+            error: false
+        });
+
+    } catch (error) {
+         next(error)
+    }
+
+   
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,106 @@
-import React from 'react'
 
-export default function Login() {
-  return (
-    <div>Login</div>
-  )
+import { useState } from "react";
+import { PiUserCircleThin } from "react-icons/pi";
+import { TbEyeCheck, TbEyeClosed,  } from "react-icons/tb"; 
+import { Link, useNavigate } from "react-router-dom";
+import {toast } from 'react-toastify'
+
+
+
+ 
+const Login = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({   
+      email: '',
+      password: '',
+      confirmPassword: ''
+    });
+   
+  
+const handleChange = (e) => {
+  setFormData({...formData, [e.target.id]: e.target.value});
+  
 }
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+ 
+  setLoading(true)
+  const response = await fetch('/api/auth/signin', {
+  method: 'POST',
+  credentials: 'include',
+  headers: {'content-type': 'application/json'},
+  body: JSON.stringify(formData)
+  })
+ try {
+  const data = await response.json();
+  
+  if(data.success){
+    setLoading(false);
+    navigate('/')
+    toast.success(data.message)
+    
+  }else{
+    setLoading(false);
+    toast.error(data.message)
+  }
+ } catch (error) {
+  setLoading(false); 
+  toast.error(error.message)
+ }
+}
+
+  return (
+    <section id="signin">
+        <div className="mx-auto container p-4">
+        <h1 className="text-center text-3xl font-semibold text-slate-700 my-5">Login</h1>
+                <div className="bg-white w-full max-w-md mx-auto rounded-md p-5">
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-5">
+                       
+                        <div className="">
+                            <label htmlFor="email">Email:</label>
+                            <div className="bg-slate-100 p-3">
+                              <input 
+                                    id="email"
+                                    onChange={handleChange} 
+                                    type="email" 
+                                    placeholder="Enter email" 
+                                    required
+                                    className="w-fll h-full outline-none bg-transparent"/>
+                            </div>
+                        </div>
+
+                        <div className="mb-5">
+                            <label htmlFor="password">Password:</label>
+                            <div className="bg-slate-100 p-3 flex justify-between">
+                               <input type={showPassword ? "text" : "password" }
+                                      placeholder="Enter password" 
+                                      className="w-fll h-full outline-none bg-transparent" 
+                                      id="password"
+                                      name="password"
+                                      onChange={handleChange}
+                                      required  
+                                      />
+
+                               <div className="cursor-pointer text-xl">
+                                 <span onClick={()=> setShowPassword((prev)=> !prev)}> 
+                                 {
+                                    showPassword ? <TbEyeClosed /> :  <TbEyeCheck />
+                                 }
+                                 </span>
+                               </div>
+                            </div> 
+                        </div>
+
+                        <button disabled={loading} type="submit" className="p-3 w-full bg-blue-600 hover:bg-blue-700 cursor-pointer text-white text-md rounded-lg disabled:opacity-80">{loading ? ('Loading...') : ('Login')}</button>
+                    </form>
+                    <p className="p-4">Already have account? <Link to='/sign-up' className="cursor-pointer text-blue-500 hover:text-blue-700 hover:underline">Sign Up</Link></p>
+                </div>
+        </div>
+    </section>
+  )
+} 
+ 
+export default Login
