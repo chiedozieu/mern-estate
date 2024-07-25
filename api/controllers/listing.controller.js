bb31d085e6f4c7fd973e07006d36c307a271ce75
chiedozieu <chiedozieugwor@gmail.com>
2024-07-25 14:27:24 +0100

    complete update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,7 +10,8 @@ export const createListing = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-};  
+}; 
+
 export const deleteListing = async (req, res, next) => {
     
     const listing = await ListingModel.findById(req.params.id)
@@ -27,5 +28,28 @@ export const deleteListing = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};  
+ 
+export const updateListing = async (req, res, next) => {
+    const listing = await ListingModel.findById(req.params.id)
+    if(!listing) {
+        return next(errorHandler(404, "listing not found"))
+    }
+    if(req.user.id !== listing.userRef){
+        return next(new errorHandler(401, 'You do not have permission to update this listing'));
+    }
+
+   try {
+
+    const updatedListing = await ListingModel.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true}
+    )
+    res.status(200).json(updatedListing)
+    
+   } catch (error) {
+    next(error); 
+   }
 };  
  
