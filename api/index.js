ac4bb953cc66beb402f907f9643af520826320ee
chiedozieu <chiedozieugwor@gmail.com>
2024-08-14 15:39:52 +0100

    pre installation

--- api/index.js
+++ api/index.js
@@ -5,9 +5,16 @@ import authRoute from './routes/auth.route.js';
 import userRoute from './routes/user.route.js'
 import listingRoute from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path'
 
 
 dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Mongoose connected')
+}).catch(error => console.log('Error connecting to database: ' + error));
+
+const __dirname = path.resolve()   // for deployment
 
 const app = express();
 app.use(cookieParser())
@@ -20,14 +27,21 @@ app.listen(3000, () => {
     console.log('Server listening on port 3000');
 })
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('Mongoose connected')
-}).catch(error => console.log('Error connecting to database: ' + error));
 
 
 app.use('/api/auth', authRoute)
 app.use('/api/user', userRoute)
 app.use('/api/listing', listingRoute)
+
+// for deployment
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+
+
 
 // middleware for error
 
