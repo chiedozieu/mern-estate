979b296449854343ddb116203486851b8181d9be
chiedozieu <chiedozieugwor@gmail.com>
2024-08-15 09:23:08 +0100

    changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -11,15 +11,15 @@ export default function Footer() {
     <div className='h-80 w-full shadow-md'>
         <div className="h-full w-full">
             <div className=" flex flex-col justify-between py-6 bg-blue-600 h-64 w-full shadow-2xl">
-                <div className="p-4 text-lg">
-                    <div className='flex flex-col text-white'>
-                        <Link to={'/search?agreementType=rent'}>Rent</Link>
-                        <Link to={'/search?agreementType=sale'}>Sale</Link>
-                        <Link to={'/search?type=land'}>Land</Link>
-                        <Link to='/about'>About</Link>
+                <div className="px-10 text-lg">
+                    <div className='flex flex-col text-white max-w-fit'>
+                        <Link to={'/search?agreementType=rent'} className='hover:underline'>Rent</Link>
+                        <Link to={'/search?agreementType=sale'} className='hover:underline'>Sale</Link>
+                        <Link to={'/search?type=land'} className='hover:underline'>Land</Link>
+                        <Link to='/about' className='hover:underline'>About</Link>
                     </div>
                 </div>
-                <div className="flex items-center justify-end p-4 gap-2">
+                <div className="flex items-center justify-end px-10 py-6 gap-2">
                     <Link>
                         <CiFacebook className='text-4xl text-white cursor-pointer hover:scale-125 transition-all duration-200'/>
                     </Link>
@@ -36,9 +36,9 @@ export default function Footer() {
             </div>
 
 
-            <div className="bg-blue-500 h-16 w-full p-2">
-            <Link to='/' className='flex items-center justify-center'>
-                <div className='relative '>
+            <div className="bg-blue-500 h-16 w-full p-2 shadow-2xl flex items-center justify-center">
+            <Link to='/' className='max-w-fit'>
+                <div className='relative'>
                     <span className='text-white font-base text-sm md:text-2xl'>Poco</span>
                     <span className='text-white font-extrabold text-base md:text-4xl'>Estate</span>
                     <div className='hidden sm:inline-flex text-purple-200 text-2xl absolute left-full rounded-full w-5 h-5'> 
